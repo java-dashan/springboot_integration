664a021079064bc96ac818be6da561e0048f20af
275554701@qq.com <275554701@qq.com>
2020-09-12 16:00:36 +0800

    dubbo 整合

---
+++ demo/dubbo/dubbo-api/src/main/java/com/huazhi/service/ISayService.java
@@ -0,0 +1,5 @@
+package com.huazhi.service;
+
+public interface ISayService {
+    String sayHello();
+}
---
+++ demo/dubbo/duboo-server/src/main/java/com/huazhi/service/ISayServiceImpl.java
@@ -0,0 +1,14 @@
+package com.huazhi.service;
+
+
+import org.apache.dubbo.config.annotation.DubboService;
+
+@DubboService(protocol = "dubbo")
+public class ISayServiceImpl implements ISayService {
+    @Override
+    public String sayHello() {
+
+
+        return "第一个接口";
+    }
+}
